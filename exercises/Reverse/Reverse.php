caab6a628d8d3f87b4f20edb794035dc0d0379b1
Roman Yepanchenko <podstavat@gmail.com>
2019-05-07 12:40:34 +0600

    Reverse complete

--- exercises/Reverse/Reverse.php
+++ exercises/Reverse/Reverse.php
@@ -19,4 +19,68 @@ namespace Exercises\Reverse;
  */
 final class Reverse
 {
+    /**
+     * @param int $number
+     * @return Int
+     */
+    public static function int(int $number): Int
+    {
+        $isNegativeInt = self::isNegativeInt($number);
+        $numbers = self::numbersMap($number);
+        $response = '';
+
+        foreach ($numbers as $reverseSubNumber) {
+            $response .= $reverseSubNumber;
+        }
+
+        if ($isNegativeInt) {
+            $response *= -1;
+        }
+
+        return (int)$response;
+    }
+
+    /**
+     * @param $number
+     * @return array
+     */
+    public static function numbersMap($number): array
+    {
+        return array_reverse(array_filter(str_split((string)$number), function ($subNumber) {
+            // if (int) make all non integer chars is zero
+            return !((int)$subNumber === 0);
+        }));
+    }
+
+    /**
+     * @param int $int
+     * @return bool
+     */
+    public static function isNegativeInt(int $int): bool
+    {
+        return $int < 0;
+    }
+
+    /**
+     * @param string $string
+     * @return string
+     */
+    public static function string(string $string): string
+    {
+        $response = '';
+        foreach (self::stringMap($string) as $char) {
+            $response .= $char;
+        }
+
+        return $response;
+    }
+
+    /**
+     * @param string $string
+     * @return array
+     */
+    public static function stringMap(string $string): array
+    {
+        return array_reverse(str_split($string));
+    }
 }
--- tests/Reverse/ReverseTest.php
+++ tests/Reverse/ReverseTest.php
@@ -12,7 +12,7 @@ final class ReverseTest extends TestCase
 {
     public function testHasMethods(): void
     {
-        self::markTestSkipped();
+   //     self::markTestSkipped();
         self::assertTrue(
             method_exists(Reverse::class, 'int'),
             'Class does not have static method int'
@@ -25,43 +25,43 @@ final class ReverseTest extends TestCase
 
     public function testCanReverseInt(): void
     {
-        self::markTestSkipped();
+     //   self::markTestSkipped();
         self::assertSame(21, Reverse::int(12));
     }
 
     public function testCanReverseIntWithThreeDigits(): void
     {
-        self::markTestSkipped();
+     //   self::markTestSkipped();
         self::assertSame(321, Reverse::int(123));
     }
 
     public function testCanReverseIntZeros(): void
     {
-        self::markTestSkipped();
+     //   self::markTestSkipped();
         self::assertSame(3, Reverse::int(300));
     }
 
     public function testCanReverseNegativeInt(): void
     {
-        self::markTestSkipped();
+     //   self::markTestSkipped();
         self::assertSame(-21, Reverse::int(-12));
     }
 
     public function testCanReverseNegativeIntWithZeros(): void
     {
-        self::markTestSkipped();
+      //  self::markTestSkipped();
         self::assertSame(-21, Reverse::int(-120));
     }
 
     public function testCanReverseString(): void
     {
-        self::markTestSkipped();
+      //  self::markTestSkipped();
         self::assertSame('ytrewq', Reverse::string('qwerty'));
     }
 
     public function testCanReverseStringWithWhitespace(): void
     {
-        self::markTestSkipped();
+      // self::markTestSkipped();
         self::assertSame('ytr ewq ', Reverse::string(' qwe rty'));
     }
 }
